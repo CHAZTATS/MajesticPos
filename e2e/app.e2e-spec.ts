7a4f2bfca313a384cc98674411f933f4d58354dd
Angular CLI <angular-cli@angular.io>
2017-06-30 10:54:10 +0100

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MajesticPosPage } from './app.po';
+
+describe('majestic-pos App', () => {
+  let page: MajesticPosPage;
+
+  beforeEach(() => {
+    page = new MajesticPosPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
